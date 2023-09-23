1f30ea5c4bd7562b64319f00bfb75c062eeda352
mkobaa <mohamed.kobaa@etu.uae.ac.ma>
2023-09-23 23:01:00 +0100

    putnbr

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		if (nb == -2147483648)
+		{
+			write(1, "-2147483648", 11);
+			return ;
+		}
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+		return ;
+	}
+	ft_putnbr(nb / 10);
+	ft_putnbr(nb % 10);
+}
+
+
+int main()
+{
+	ft_putnbr(-50);
+}
